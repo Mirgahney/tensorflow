CodeLinaro-mirror/le_external_github.com_tensorflow_tensorflow

Move kTfLiteReducedPrecisionKey to a separate header file.
PiperOrigin-RevId: 647095526

tensorflow/compiler/mlir/lite/sparsity/sparsify_model.cc
@@ -33,11 +33,11 @@ limitations under the License.
 #include "tensorflow/compiler/mlir/lite/flatbuffer_export.h"
 #include "tensorflow/compiler/mlir/lite/flatbuffer_import.h"
 #include "tensorflow/compiler/mlir/lite/schema/schema_generated.h"
+#include "tensorflow/compiler/mlir/lite/tools/optimize/reduced_precision_key.h"
 #include "tensorflow/compiler/mlir/lite/transforms/passes.h"
 #include "tensorflow/compiler/mlir/lite/utils/convert_type.h"
 #include "tensorflow/compiler/mlir/tensorflow/utils/error_util.h"
 #include "tensorflow/core/framework/types.pb.h"
-#include "tensorflow/lite/tools/optimize/reduced_precision_support.h"
 
 namespace mlir {
 namespace lite {

tensorflow/compiler/mlir/lite/sparsity/sparsify_model_test.cc
@@ -28,8 +28,8 @@ limitations under the License.
 #include "absl/status/status.h"
 #include "flatbuffers/flatbuffer_builder.h"  // from @flatbuffers
 #include "tensorflow/compiler/mlir/lite/schema/schema_generated.h"
+#include "tensorflow/compiler/mlir/lite/tools/optimize/reduced_precision_key.h"
 #include "tensorflow/lite/core/model_builder.h"
-#include "tensorflow/lite/tools/optimize/reduced_precision_support.h"
 
 namespace mlir {
 namespace lite {

tensorflow/lite/tools/optimize/reduced_precision_support.h
@@ -17,13 +17,11 @@ limitations under the License.
 
 #include <string>
 
+#include "tensorflow/compiler/mlir/lite/tools/optimize/reduced_precision_key.h"
 #include "tensorflow/lite/kernels/internal/compatibility.h"
 
 namespace tflite {
 namespace optimize {
-
-static constexpr char kTfLiteReducedPrecisionKey[] =
-    "reduced_precision_support";
 
 static constexpr char kTfLiteFloat16String[] = "fp16";
 static constexpr char kTfLiteBfloat16String[] = "bf16";

Mirgahney/tensorflow

tensorflow/compiler/mlir/lite/sparsity/sparsify_model.cc
@@ -33,11 +33,11 @@ limitations under the License.
 #include "tensorflow/compiler/mlir/lite/flatbuffer_export.h"
 #include "tensorflow/compiler/mlir/lite/flatbuffer_import.h"
 #include "tensorflow/compiler/mlir/lite/schema/schema_generated.h"
+#include "tensorflow/compiler/mlir/lite/tools/optimize/reduced_precision_core.h"
 #include "tensorflow/compiler/mlir/lite/transforms/passes.h"
 #include "tensorflow/compiler/mlir/lite/utils/convert_type.h"
 #include "tensorflow/compiler/mlir/tensorflow/utils/error_util.h"
 #include "tensorflow/core/framework/types.pb.h"
-#include "tensorflow/lite/tools/optimize/reduced_precision_support.h"
 
 namespace mlir {
 namespace lite {

tensorflow/compiler/mlir/lite/sparsity/sparsify_model_test.cc
@@ -28,8 +28,8 @@ limitations under the License.
 #include "absl/status/status.h"
 #include "flatbuffers/flatbuffer_builder.h"  // from @flatbuffers
 #include "tensorflow/compiler/mlir/lite/schema/schema_generated.h"
+#include "tensorflow/compiler/mlir/lite/tools/optimize/reduced_precision_core.h"
 #include "tensorflow/lite/core/model_builder.h"
-#include "tensorflow/lite/tools/optimize/reduced_precision_support.h"
 
 namespace mlir {
 namespace lite {

tensorflow/lite/tools/optimize/reduced_precision_support.h
@@ -17,13 +17,11 @@ limitations under the License.
 
 #include <string>
 
+#include "tensorflow/compiler/mlir/lite/tools/optimize/reduced_precision_core.h"
 #include "tensorflow/lite/kernels/internal/compatibility.h"
 
 namespace tflite {
 namespace optimize {
-
-static constexpr char kTfLiteReducedPrecisionKey[] =
-    "reduced_precision_support";
 
 static constexpr char kTfLiteFloat16String[] = "fp16";
 static constexpr char kTfLiteBfloat16String[] = "bf16";